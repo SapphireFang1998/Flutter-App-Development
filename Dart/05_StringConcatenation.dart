1f452cdb360232e518f5f6170305a00488cff885
SapphireFang2000 <n.a.refat2000@gmail.com>
2024-08-19 03:34:36 +0600

    String Concatenation

---
+++ Dart/05_StringConcatenation.dart
@@ -0,0 +1,17 @@
+void main()
+{
+  var id = 101;
+  var name = 'Shamim';
+  var age = 25;
+
+  print('ID is $id, Name is $name, Age is $age');
+  print('\r'); //Printing new line
+
+  var value1 = 10;
+  var value2 = 5;
+
+  print('$value1 + $value2');
+  print('\r'); //Printing new line
+
+  print('$value1 + $value2 = ${value1+value2}');
+}
